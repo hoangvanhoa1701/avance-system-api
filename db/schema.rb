92928433e08970cfbf941c640b43fe17e9f9dcab
hoangvanhoa1701 <hoangvanhoa1701@gmail.com>
2021-12-02 18:17:10 +0700

    feat: add schema, user validates and role enum

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_02_050213) do
+ActiveRecord::Schema.define(version: 2021_12_02_050444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.datetime "deleted_at"
+    t.bigint "session_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["session_id"], name: "index_categories_on_session_id"
+  end
+
+  create_table "lessons", force: :cascade do |t|
+    t.string "title"
+    t.string "link"
+    t.string "file"
+    t.datetime "deleted_at"
+    t.bigint "session_id", null: false
+    t.bigint "category_id", null: false
+    t.bigint "unit_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_lessons_on_category_id"
+    t.index ["session_id"], name: "index_lessons_on_session_id"
+    t.index ["unit_id"], name: "index_lessons_on_unit_id"
+  end
+
+  create_table "presentations", force: :cascade do |t|
+    t.string "link"
+    t.string "file"
+    t.datetime "deleted_at"
+    t.bigint "lesson_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["lesson_id"], name: "index_presentations_on_lesson_id"
+  end
 
   create_table "programs", force: :cascade do |t|
     t.string "title"
@@ -31,6 +65,37 @@ ActiveRecord::Schema.define(version: 2021_12_02_050213) do
     t.index ["user_id"], name: "index_programs_users_on_user_id"
   end
 
+  create_table "resources", force: :cascade do |t|
+    t.string "link"
+    t.string "file"
+    t.datetime "deleted_at"
+    t.bigint "lesson_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["lesson_id"], name: "index_resources_on_lesson_id"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "title"
+    t.datetime "deleted_at"
+    t.bigint "program_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["program_id"], name: "index_sessions_on_program_id"
+  end
+
+  create_table "units", force: :cascade do |t|
+    t.string "title"
+    t.string "link"
+    t.datetime "deleted_at"
+    t.bigint "category_id", null: false
+    t.bigint "session_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_units_on_category_id"
+    t.index ["session_id"], name: "index_units_on_session_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
@@ -44,4 +109,13 @@ ActiveRecord::Schema.define(version: 2021_12_02_050213) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "categories", "sessions"
+  add_foreign_key "lessons", "categories"
+  add_foreign_key "lessons", "sessions"
+  add_foreign_key "lessons", "units"
+  add_foreign_key "presentations", "lessons"
+  add_foreign_key "resources", "lessons"
+  add_foreign_key "sessions", "programs"
+  add_foreign_key "units", "categories"
+  add_foreign_key "units", "sessions"
 end
